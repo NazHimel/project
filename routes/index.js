06a9e309311f02487a71b8f7c00f7746165a1ea5
Himel <nazmulhasanhimel@yahoo.com>
2019-06-16 21:42:59 +0600

    returned to ce8d56b0216f0bc240d6b20d144e6f08a26c0f6d commit, not changing the index.js

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ let auth = require('../services/auth');
 module.exports = function (app) {
   app.use('/', require('./home'));
   app.use('/sample', auth.isAuthenticated, require('./sample'));
-  app.use('/api/users', require('./users'));
+  app.use('/users', require('./users'));
 };
